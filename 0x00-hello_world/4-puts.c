6b3caa91a650c57fe0c488332b79f9ce95d22a8e
Victor Osibajo <victoroshi@yahoo.com>
2023-05-21 23:47:17 +0000

    using put funtion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 /**
+* This is the first line of code to 
+* demontstrate the betty 
+* style of coding
 * Description: Write a C program that prints exactly
 * "Programming is like building a multilingual puzzle,
 * followed by a new line
