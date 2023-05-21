a2c0d7a8e252a345d44be8ffeca9bd18ca114d21
Victor Osibajo <victoroshi@yahoo.com>
2023-05-21 22:49:41 +0000

    using put funtion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*  C program that prints */
+/*C program that prints string and return value*/
 int main(void)
 {
 	char print_string[] = "\"Programming is like building a multilingual puzzle";
