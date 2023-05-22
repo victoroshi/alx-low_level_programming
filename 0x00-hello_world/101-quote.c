c5a99aff42add43b8472bb0cfcf8c8b4bd352856
Victor Osibajo <victoroshi@yahoo.com>
2023-05-22 01:48:31 +0000

    print as displayed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - program that prints exactly as displayed
+* Return: always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
