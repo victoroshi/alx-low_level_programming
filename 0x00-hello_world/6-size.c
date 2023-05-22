5e04d467a7b09e53072f5a761c4ebcadd9303490
Victor Osibajo <victoroshi@yahoo.com>
2023-05-22 01:26:58 +0000

    print size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
-* main - prints a text using printf function
-* Return: always 0
+* main - prints the size of various types
+* Return: Always 0 (Success)
 */
 int main(void)
 {
