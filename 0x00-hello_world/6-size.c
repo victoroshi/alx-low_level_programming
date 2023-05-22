6370d9bac4bf7fdf63c8a224425fafc04258ce14
Victor Osibajo <victoroshi@yahoo.com>
2023-05-22 00:33:28 +0000

    print size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
-#include<limits.h>
 #include<stdio.h>
+#include<sys/utsname.h>
 /**
 * main - prints a text using printf function
 * Return: always 0
 */
 int main(void)
 {
-	printf("%d bits\n", (int)(CHAR_BIT * sizeof(void *)));
+	struct utsname a;
+	uname (&a);
 	return (0);
 }
