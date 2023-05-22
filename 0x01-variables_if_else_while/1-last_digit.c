70a986557a070c0d90fb5059d52d80e6acf98df7
Victor Osibajo <victoroshi@yahoo.com>
2023-05-22 19:54:10 +0000

    assign a random number to the variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,8 @@
 */
 int main(void)
 {
-	int n, m;
+	int n; 
+	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -21,7 +22,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", m, m);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	else
 	{
