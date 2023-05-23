b3e8727102235788e2dec3356f3888d230a3467c
Victor Osibajo <victoroshi@yahoo.com>
2023-05-23 23:06:58 +0000

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: program that prints _putchar, 
+* followed by a new line.
+* Return: Always (0)
+
+int main(void)
+{
+	char t = '_putchar';
+	putchar(t);
+	return (0);
+}
