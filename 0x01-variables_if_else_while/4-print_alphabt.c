d8962a3143383d2019a656defe15dc765e989e59
Victor Osibajo <victoroshi@yahoo.com>
2023-05-23 00:20:48 +0000

    prints the alphabet in lowercase, followed by a new line except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet in lowercase, followed by a new line
+* Return: always 0
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
