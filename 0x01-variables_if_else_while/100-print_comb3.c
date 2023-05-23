cbaa622b10ec6463259968eeb415a1ef02b39df3
Victor Osibajo <victoroshi@yahoo.com>
2023-05-23 15:56:13 +0000

    program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (i != j) /* Exclude any combinations with the same digit */
+			if (i != '8' || j != '9') /* Exclude any combinations with the same digit */
 			{
 				putchar('0' + i); /* print the first digit */
 				putchar('0' + j); /* print the second digit */
