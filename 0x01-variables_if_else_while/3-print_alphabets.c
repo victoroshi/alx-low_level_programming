ab2ba1d495398cc948b3e63f2955729faeefc9c5
Victor Osibajo <victoroshi@yahoo.com>
2023-05-23 00:08:52 +0000

    print lowercase follow by UPPERCASE

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet in lowercase, 
+* followed by uppercase character
+* Return: always 0
+*/
+int main(void)
+{
+	char alpha, uppercase;
+
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+	{
+	putchar(alpha);
+	}
+	for (uppercase = 'A'; uppercase <= 'Z' ; uppercase++)
+	{
+	putchar(uppercase);
+	}
+	putchar('\n');
+	return (0);
+}
