5a1a0ef815d798898f211a7d228a41e60e70215d
Victor Osibajo <victoroshi@yahoo.com>
2023-05-24 20:17:16 +0000

    prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - entry point
+* Description: program that prints lowercase alphabet.
+* followed by a new line.
+* Return: Always (0)
+*/
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+	return (0);
+}
