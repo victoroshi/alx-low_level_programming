89ecf891501cde98f684eca1c5d8e24523b61c09
Victor Osibajo <victoroshi@yahoo.com>
2023-05-25 18:43:12 +0000

    tests function that prints if integer is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+* main - program that prints randon numbers
+* Return: always 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,7 @@
+### C - Debugging
+- Debugging
+- Rubber Duck Debugging
+
+Debugging is the process of finding and fixing errors in software that prevents it from running correctly. 
+As you become a more advanced programmer and an industry engineer, you will learn how to use debugging tools 
+such as gdb or built-in tools that IDEs have. However, it’s important to understand the concepts and processes of debugging manually.
