447dd138a017d4f3d70756bebc7eba41068077e7
Victor Osibajo <victoroshi@yahoo.com>
2023-05-25 10:52:45 +0000

    prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+  * print_to_98 - print digit number from  0 t0 98
+  * @num: this is the starting index
+  * Return: Returns the num
+  */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	}
+
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+
+	printf("%d\n", n);
+}
