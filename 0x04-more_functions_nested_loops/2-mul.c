afea2ddecddf07e50837d01d6eae50deb685e608
Victor Osibajo <victoroshi@yahoo.com>
2023-05-27 04:44:55 +0000

    checking alphabets and numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integer values
+ * @a: char one
+ * @b: char two that will be multplied with one
+ * Return: return the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
