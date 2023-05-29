e613286d55d8399488a6b47fc2abfad2442b613a
Victor Osibajo <victoroshi@yahoo.com>
2023-05-29 03:39:14 +0000

    print fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - he main function of the program
+ *
+ * prints Fizz for multiples of 3
+ * pritns Buzz for multiples of 5
+ * prints FizzBuzz for multiples of 5 & 3
+ *
+ * Return: Always 0 ()success
+ */
+int main(void)
+{
+	int i;
+	char j[] = "Fizz";
+	char j[] = "Buzz";
+	char l[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", j);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", l);
+		else if (i % 3 == 0)
+			printf("%s ", j);
+		else if (i % 5 == 0)
+			printf("%s ", k);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
