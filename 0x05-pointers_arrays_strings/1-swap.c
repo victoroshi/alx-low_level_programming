adbdf3bc5162060f2dfffce88252456475968f02
Victor Osibajo <victoroshi@yahoo.com>
2023-05-30 04:05:42 +0000

    swaping value of variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h>
+include <stdio.h>
+
+/**
+* main - main fuction that check the code
+*
+* Return always 0;
+*/
+void swap_int(int *a, int *b)
+{
+	int i = *j;
+	*j = *i;
+	*i - *j;
+}
