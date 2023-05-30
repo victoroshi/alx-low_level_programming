fbb797f69011d937670a2883c7051f7fa09ac5d6
Victor Osibajo <victoroshi@yahoo.com>
2023-05-30 04:57:08 +0000

    print character in reversestringr

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * puts2 - unction that prints every other character of a string,
+  * starting with the first character, followed by a new line
+  *
+  * @str: a pointer the string we want to print
+  *
+  * Return: Nothing
+  */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		if (count % 2 == 0)
+			_putchar(str[count]);
+
+		count++;
+	}
+
+	_putchar('\n');
+}
