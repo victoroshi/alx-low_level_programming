4db990c99eeeca378010dae134bfab7a0edae66e
Victor Osibajo <victoroshi@yahoo.com>
2023-05-31 03:00:44 +0000

    prints n elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - checking the main function
+* print_array - array to be check for iteration
+* @a: this is the pointer to the array
+* @n: the numbers of teh array that will be printed
+*/
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
