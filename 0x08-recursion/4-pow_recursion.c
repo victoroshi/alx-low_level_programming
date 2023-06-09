c25153754ea5e7786d2f7cddf52e411e4f72d2b5
Victor Osibajo <victoroshi@yahoo.com>
2023-06-09 02:14:56 +0000

    retunn the value of x raise to power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include  "main.h"
+
+/**
+* _pow_recursion - return the value of x raise to the power of y
+* @x: first integer parameter 
+* @y: second integer parameter
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
