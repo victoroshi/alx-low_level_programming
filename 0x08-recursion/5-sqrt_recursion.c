d9f3dea18c9ecdabe675d6879e1c52549d71bffe
Victor Osibajo <victoroshi@yahoo.com>
2023-06-09 14:47:52 +0000

    perfect square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,18 +11,3 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt2(1, n));
 }
-/**
- *_sqrt2 - find square of numbers
- *@a: an integer variable
- *@b: an integer variable
- *Return: value square
- */
-int _sqrt2(int a, int b)
-{
-	if (a * a > b)
-		return (-1);
-	else if (a * a == b)
-		return (a);
-	else
-		return (_sqrt2(a += 1, b));
-}
