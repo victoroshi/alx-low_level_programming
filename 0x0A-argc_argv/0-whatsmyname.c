48284ae6d671a2381b118bc9be14a7b34630075b
Victor Osibajo <victoroshi@yahoo.com>
2023-06-11 03:31:59 +0000

    print program file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+* main - the main program for execution
+* @argc: an integer counter for array value
+* @argv: a character sring for array elements
+*/
 int main(int argc, char* argv[])
 {
     printf("My program name is %s\n", argv[0]);
