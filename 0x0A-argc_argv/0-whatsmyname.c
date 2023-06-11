0038396c5dfd96e8832901c8827cfef35e343a02
Victor Osibajo <victoroshi@yahoo.com>
2023-06-11 03:34:39 +0000

    print program file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,6 +5,7 @@
 * main - the main program for execution
 * @argc: an integer counter for array value
 * @argv: a character sring for array elements
+* Return 0 always
 */
 int main(int argc, char* argv[])
 {
