70414ca51b192b0a5c207a2e427e83af1c69fff4
Victor Osibajo <victoroshi@yahoo.com>
2023-06-11 11:10:17 +0000

    program that prints its name, followed by a new line

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: pointer to the array element
  * Return: exit code 0
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
     printf("%d\n", argc -1);
     return (0);
