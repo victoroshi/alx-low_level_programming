dad97df61e6fdb482379e76ad19c1d56bc7d3a23
Victor Osibajo <victoroshi@gmail.com>
2023-06-23 00:20:24 +0000

    function that pass parameters

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+  * array_iterator - executes a function
+  * given as a parameter on each element of an array
+  *
+  * @size: array size
+  * @action: pointer to the function to use
+  * @array: integer array to iterate on
+  *
+  * Return: void
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (size <= 0)
+		return;
+	if (array == NULL || action == NULL)
+		return;
+
+	for (; i < size; i++)
+		action(array[i]);
+}~
