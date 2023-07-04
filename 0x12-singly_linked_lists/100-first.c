1fe6296a86eff5855041a1cd29f77bd7b5b3d961
Victor Osibajo <victoroshi@gmail.com>
2023-07-04 23:48:10 +0000

    prints characters before main funtion

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+  * _main_pred - print  teh characters before executing the main function
+  * Return: void
+  */
+void _main_pred(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
