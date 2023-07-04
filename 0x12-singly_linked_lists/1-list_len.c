2bb3b56f5946ab9a26758f4c6c8a88359942e0c5
Victor Osibajo <victoroshi@gmail.com>
2023-07-04 01:34:13 +0000

    function that prints all the elements of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - prints out the  number of elements in the singly linked list
+ * @h: pointer 
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num_node = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num_node++;
+	}
+
+	return (num_node);
+}
