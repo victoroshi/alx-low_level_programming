65f3e4cbb2bfe50df9a6ba3eff862bbf179a7110
Victor Osibajo <victoroshi@gmail.com>
2023-09-10 05:33:57 +0000

    using puts function in C to display charcaters to teh standard output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,8 @@
 
 /**
  * main - Entry point
- *
- * Description: Program to displayed characters to the standard output
- * @char: string to be displayed to the standard output
- *
- * Return always 0 on success
+ * Description: Program to display character to the standard output
+ * Return: always 0 on success
  */
 int main(void)
 {
