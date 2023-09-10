6bc4bef736518460b8c6f28da7cfe3c70c68cf83
Victor Osibajo <victoroshi@gmail.com>
2023-09-10 19:49:09 +0000

    finding the size of data types in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	printf("Size of char is: %lu bytes\n", sizeof(char));
-	printf("Size of int is: %lu bytes\n", sizeof(int));
-	printf("Size of long int is: %lu bytes\n", sizeof(long int));
-	printf("Size of long long int is: %lu bytes\n", sizeof(long long int));
-	printf("Size of float is: %lu bytes\n", sizeof(float));
+	printf("Size of a char: %lu bytes\n", sizeof(char));
+	printf("Size of a int: %lu bytes\n", sizeof(int));
+	printf("Size of a long int: %lu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes\n", sizeof(float));
 	return (0);
 }
 
