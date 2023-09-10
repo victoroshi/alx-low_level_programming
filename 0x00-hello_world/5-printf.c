a9166942baf63ba874036805bd380ff121a6f498
Victor Osibajo <victoroshi@gmail.com>
2023-09-10 05:56:39 +0000

    using printf to print strings of characters

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Using printf to print charaters
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	char myString[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", myString);
+
+	return (0);
+}
+
