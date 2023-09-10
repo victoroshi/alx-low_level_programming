df5d944e166f8d79213ca68fc891f1445fece81e
Victor Osibajo <victoroshi@gmail.com>
2023-09-10 04:40:20 +0000

    using puts function in C to display charcaters to teh standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** C program to use the puts function
+ * main - This is the entry point
+ *
+ * @char: string to be displayed to the standard output
+ *
+ * Return always 0 on success
+ */
+int main() {
+	char myString[] = "Programming is like building a multilingual puzzle";
+	puts(myString);
+
+	return (0);
+}
+
