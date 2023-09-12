200a522d31ee3d54a3cc6006a6ede6655d1068a3
Victor Osibajo <victoroshi@gmail.com>
2023-09-12 19:28:53 +0000

    prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints all possible different combinations of two digits
+ *
+ * Return: always (0)
+ */
+
+int main()
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+		putchar((char)i);
+		putchar((char)j);
+		if (i != 56 || j != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar('\n');
+	}
+	}
+	return (0);
+}
