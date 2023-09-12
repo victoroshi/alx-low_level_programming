dc2eb1ef5aef26aed2ee025e40ed90890eeb8579
Victor Osibajo <victoroshi@gmail.com>
2023-09-12 18:37:31 +0000

    printing numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: program that prints number 0 through 9.
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	for (num = 97; num <= 102; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
