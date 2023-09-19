b1b26d999321a0f879a396a1827403f4ede4b4c1
Victor Osibajo <victoroshi@gmail.com>
2023-09-19 06:53:20 +0000

    removing infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i); /* This will casue infinite loop */
-	}
+	/*while (i < 10)*/
+	/*{*/
+		/* putchar(i);*/
+	/*} */
 
 	printf("Infinite loop avoided! \\o/\n");
 
