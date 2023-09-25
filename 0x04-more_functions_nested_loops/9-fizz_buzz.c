8d9fb46e5b2535f2153f8b05615170c3175adad0
Victor Osibajo <victoroshi@gmail.com>
2023-09-25 14:51:28 +0000

    script for Fizz Buzz game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,27 +12,18 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-		i++;
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if ((i % 5 != 0) && (i % 3 != 0))
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
