bd4b04b0f83fdb129120af0d63f71f04fa4a069d
Victor Osibajo <victoroshi@gmail.com>
2023-09-27 23:46:38 +0000

    function that manipulate pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * reset_to_98 - function to update the value of a pointer
+ *
+ * @n: integer to manipulate
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
