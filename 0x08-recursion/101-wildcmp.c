a885ce468d3ce87435da19446c5caae2562f794a
Victor Osibajo <victoroshi@gmail.com>
2023-10-04 15:17:10 +0000

    fuction that check the identical of strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * wildcmp - This is the entry Point
+ * @s1: input 1
+ * @s2: input 2
+ * Return: always 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
