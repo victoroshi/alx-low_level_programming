6e570d941f2f1d68d6388a0696e074f4dc1ba247
Victor Osibajo <victoroshi@gmail.com>
2023-10-12 18:12:34 +0000

    using struct to create dog details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - identification of dog
+ * @name: pointer to character string
+ * @age: floating point integer
+ * @owner: pointer to character string
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif DOG_H /* DOG_H */
