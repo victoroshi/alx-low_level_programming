d286834bc67c457c8893f843081c743624185c51
Victor Osibajo <victoroshi@gmail.com>
2023-10-20 03:40:44 +0000

    printstrings of characters before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * _main_pred - print chars before  the function main is executed
+  *
+  * Return: void
+  */
+void _main_pred(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
